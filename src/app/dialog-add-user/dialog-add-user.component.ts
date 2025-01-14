902093d344e71d516b5e3ebd11a09c8d97c56db6
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-01-14 18:00:44 +0100

    progress

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule, MatInputModule, MatFormFieldModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
