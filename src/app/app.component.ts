3f986cb407cf354bebcd36186f647894ee421aab
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-01-14 16:36:35 +0100

    progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
@@ -10,7 +9,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   selector: 'app-root',
   imports: [
     MatToolbarModule,
-    MatButtonModule,
     MatIconModule,
     MatSidenavModule,
     RouterLink,
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatDialogModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
-
+  readonly dialog = inject(MatDialog);
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 }
