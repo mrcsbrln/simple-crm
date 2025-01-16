c042375797244e251667036f94615a8c85bdb846
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-01-16 12:03:06 +0100

    progress

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,24 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: "AIzaSyC3Fe_WeoGo4Hfy57ZL2W8wQsPFBLn6V4k",
+        authDomain: "simple-crm-84d2c.firebaseapp.com",
+        projectId: "simple-crm-84d2c",
+        storageBucket: "simple-crm-84d2c.firebasestorage.app",
+        messagingSenderId: "118627510214",
+        appId: "1:118627510214:web:e019d34a037338df81b4c7"
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,6 +7,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../models/user.class';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  addDoc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -24,6 +32,7 @@ import { User } from '../../models/user.class';
 })
 export class DialogAddUserComponent {
   user = new User();
+  private firestore: Firestore = inject(Firestore);
 
   saveUser() {
     console.log(this.user);
