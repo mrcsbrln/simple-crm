1ddca1a51f5f74a36fb472c0f85d17c112477b27
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-01-16 19:40:05 +0100

    progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent},
