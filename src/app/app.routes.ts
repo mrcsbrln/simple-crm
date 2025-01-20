5ec6644d4f1bae682c18f22f582a2f83a9a30998
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-01-20 18:12:19 +0100

    user-details-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UserDetailsComponent } from './components/user/user-details/user-details.component';
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent},
-    {path: 'user', component: UserComponent},
     {path: 'dashbboard', component: DashboardComponent},
+    {path: 'user', component: UserComponent},
+    {path: 'user/:id', component: UserDetailsComponent},
 ];
