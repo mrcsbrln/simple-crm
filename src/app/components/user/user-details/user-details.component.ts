cba64d53a22e9d5bad1adfdf14c7e210600bf8da
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-01-24 15:12:22 +0100

    progress

--- src/app/components/user/user-details/user-details.component.ts
+++ src/app/components/user/user-details/user-details.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../../../services/user.service';
+import { User } from '../../../interfaces/user.interface';
 
 @Component({
   selector: 'app-user-details',
@@ -9,17 +10,19 @@ import { UserService } from '../../../services/user.service';
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss',
 })
-export class UserDetailsComponent implements OnInit {
+export class UserDetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  userService: UserService = inject(UserService)
+  userService: UserService = inject(UserService);
   userId: string = '';
-  currentUser: any;
+  currentUser: User | undefined;
 
-  ngOnInit(): void {
+  constructor() {
     this.route.paramMap.subscribe((params) => {
       this.userId = params.get('id') || '';
       console.log('User ID:', this.userId);
     });
-    this.currentUser = this.userService.users.find(user => user.id === this.userId);
+    this.currentUser = this.userService.users.find(
+      (user) => user.id === this.userId
+    );
   }
 }
